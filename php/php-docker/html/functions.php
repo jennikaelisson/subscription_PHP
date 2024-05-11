ac60274c788972ecb19bd231588d52f942972306
Jennika Elisson <jennika.elisson@gmail.com>
2024-05-11 11:47:04 +0200

    moved functions to their own file

--- php/php-docker/html/functions.php
+++ php/php-docker/html/functions.php
@@ -4,18 +4,6 @@
         return $title.' - My awesome title';
     }
 
-    function user_has_role($role) {
-        // customer or subscriber
-        if ($role) {
-            return $role;
-        }
-        return "Not valid role";
-    }
-
-    function is_signed_in() {
-
-        //return true or false
-    }
 
 
 ?>

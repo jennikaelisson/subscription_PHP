d447760d05cdc14ba7c59d56c7677bb46ce98943
Jennika Elisson <jennika.elisson@gmail.com>
2024-05-12 10:40:03 +0200

    check what role user has

--- php/php-docker/html/functions/authFunctions.php
+++ php/php-docker/html/functions/authFunctions.php
@@ -1,36 +1,29 @@
-<?php 
-// SKA INTE LIGGA HÄR 
-    // if (isset($_SESSION['role'])) {
-    //         $role_from_session = $_SESSION['role'];
-    //     } else {
-    //         $role_from_session = null;
-    //     }
+<?php
+session_start();
 
-    // $role = user_has_role($role_from_session);
-
-    function user_has_role($role) {
-        if (!empty($role)) {
-            return $role;
-        }
-        return "Not valid role";
+function user_has_role($role) {
+    if (!empty($role)) {
+        return $role;
     }
+    return "Not valid role";
+}
 
-    // SKA INTE LIGGA HÄR
-    // if (isset($_SESSION['auth'])) {
-    //     $auth_from_session = $_SESSION['auth'];
-    // } else {
-    //     $auth_from_session = null;
-    // }
-
-    // $auth = is_signed_in($auth_from_session);
-
-    function is_signed_in($auth) {
-        if ($auth === true) {
-            return true;
-        }
-        return false;
+function is_signed_in($auth) {
+    if ($auth === true) {
+        return true;
     }
-    
+    return false;
+}
 
+function require_role($role) {
+    // Kontrollera om användaren är inloggad och har rätt roll
+    if (!isset($_SESSION['auth']) || !isset($_SESSION['role']) || !is_signed_in($_SESSION['auth']) || $_SESSION['role'] !== $role) {
+        // Användaren har inte rätt roll eller är inte inloggad, gör en omdirigering
+        header("Location: /no-access.php");
+        exit(); // Avsluta scriptet efter omdirigering
+    }
+}
 
+// Använda require_role-funktionen för att kontrollera att användaren har rollen 'customer'
+require_role('customer');
 ?>

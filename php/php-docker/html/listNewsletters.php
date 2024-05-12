5eaeac4cf9a31e481c31d72ab18284ac6fed5fab
Jennika Elisson <jennika.elisson@gmail.com>
2024-05-12 10:38:35 +0200

    list all newsletters

--- php/php-docker/html/listNewsletters.php
+++ php/php-docker/html/listNewsletters.php
@@ -1,4 +1,12 @@
 <?php
+    session_start();
+
+    $mysqli = new mysqli("db", "root", "notSecureChangeMe", "assignment2");
+
+    $sql = "SELECT * FROM newsletters";
+$result = $mysqli->query($sql);
+?>
+<?php
     include_once('functions.php');
 ?>    
 <?php
@@ -6,12 +14,25 @@
     include('header.php');
 ?>
 
-
-    <div>
+<html>
+    <body>
+        <div>
         <?php echo("All newsletters"); ?>
     </div>
-
-
+        <?php
+        // Loopa genom varje nyhetsbrev och visa dem
+        while ($row = $result->fetch_assoc()) {
+            ?>
+            <div style="border: 1px solid #000000">
+                <h3><?php echo $row['title']; ?></h3>
+                <p><?php echo $row['description']; ?></p>
+                <p>Owner: <?php echo $row['owner']; ?></p>
+            </div>
+            <?php
+        }
+        ?>
+    </body>
+    </html>
 
 <?php
     include('footer.php');

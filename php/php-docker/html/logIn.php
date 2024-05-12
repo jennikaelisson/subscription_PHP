8716c42e2753648447055d7f4be7cf6b3b396359
Jennika Elisson <jennika.elisson@gmail.com>
2024-05-12 10:39:19 +0200

    check if user exists in database

--- php/php-docker/html/logIn.php
+++ php/php-docker/html/logIn.php
@@ -3,12 +3,30 @@ include_once('functions.php');
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['role'])) {
-        $_SESSION['role'] = $_POST['role'];
-    }
 
-    if (isset($_POST['auth'])) {
-        $_SESSION['auth'] = $_POST['auth'];
+    $mysqli = new mysqli("db", "root", "notSecureChangeMe", "assignment2");
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    $result = $mysqli->query($sql);
+
+    if ($result->num_rows > 0) {
+       // Användaren finns i databasen, så spara deras uppgifter i sessionen
+       $_SESSION['email'] = $email;
+       $_SESSION['password'] = $password;
+       
+       // Sätt roll och autentiseringsstatus i sessionen
+       $_SESSION['role'] = 'customer';
+       $_SESSION['auth'] = 'true';
+        
+        // Utför eventuella andra åtgärder, t.ex. omdirigering
+        header('Location: ' . $_SERVER['REQUEST_URI']);
+        exit();
+    } else {
+        // Användaren finns inte i databasen, hantera detta scenario (t.ex. visa felmeddelande)
+        echo "Felaktiga användaruppgifter. Försök igen.";
     }
     var_dump($_SESSION);
     header('Location: ' . $_SERVER['REQUEST_URI']);
@@ -26,6 +44,7 @@ include('header.php');
     <form method="POST">
         <input type="hidden" name="role" value="customer">
         <input type="hidden" name="auth" value="true">
+        <input type="email" name="email" value="jennika.elisson@gmail.com">
         <input type="password" name="password" value="123456">
         <input type="submit">
     </form>

53b5df3e19c3d080b576276a6c42e1c8355d0725
Jennika Elisson <jennika.elisson@gmail.com>
2024-05-16 12:30:25 +0200

    register form

--- php/php-docker/html/createAccount.php
+++ php/php-docker/html/createAccount.php
@@ -5,12 +5,20 @@
     $title = "My page";
     include('header.php');
 ?>
-
-
     <div>
         <?php echo("Create account"); ?>
     </div>
 
+    <form action="post">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
+        <label for="role">Select Role:</label>
+        <select name="role" id="role">
+            <option value="customer">Customer</option>
+            <option value="subscriber">Subscriber</option>
+        </select>
+        <input type="submit" name="submit" value="Create Account">
+    </form>
 
 
 <?php

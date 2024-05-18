fb9999f74642d954e6cb376750775371763ca3ab
Jennika Elisson <jennika.elisson@gmail.com>
2024-05-18 18:19:05 +0200

    added footer section

--- php/php-docker/html/footer.php
+++ php/php-docker/html/footer.php
@@ -1,6 +1,16 @@
-        <footer>
-            <h2>footer</h2>footer
-        </footer>
+<footer>
+    <div class="footer-content">
+        <p>&copy; <?php echo date("Y"); ?> My Website. All rights reserved.</p>
+        <p>Follow us on:
+            <a href="https://www.facebook.com" target="_blank">Facebook</a> |
+            <a href="https://www.twitter.com" target="_blank">Twitter</a> |
+            <a href="https://www.instagram.com" target="_blank">Instagram</a>
+        </p>
+    </div>
+</footer>
+
+
+
     
     </body>
 </html>

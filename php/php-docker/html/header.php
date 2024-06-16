1543f65a542633e41644521d049a3cfb6bb4ffb0
Jennika Elisson <jennika.elisson@gmail.com>
2024-06-16 13:25:51 +0200

    fixed header - auth

--- php/php-docker/html/header.php
+++ php/php-docker/html/header.php
@@ -1,41 +1,50 @@
-<?php include('auth.php'); ?>
-<html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="styles.css?v=<?php echo time(); ?>">
-        <title>
-            <?php echo(get_window_title($title)); ?>
-        </title>
-        <style>
-            .list-item {
-                border: 2px dotted plum;
-            }
-            a {
-                color: purple;
-            }
-        </style>
-    </head>
-    <body>
-        <header>
-            <h2>Groovy Times Collective</h2>
-            <nav><?php
-            var_dump($isLoggedIn);  // See if the user is logged in
-                var_dump($userRole);    // See the user's role
+<?php 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
-            <?php if ($isLoggedIn): ?>
-                <?php if ($userRole === 'customer'): ?>
-                    <a href="mySubscribers.php">My subscribers</a>
-                    <a href="myNewsletter.php">My newsletters</a>
-                    <a href="logOut.php">Log out</a>
-                <?php elseif ($userRole === 'subscriber'): ?>
-                    <a href="listNewsletters.php">All newsletters</a>
-                    <a href="mySubscriptions.php">My subscriptions</a>
-                    <a href="logOut.php">Log out</a>
-                <?php endif; ?>
-            <?php else: ?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css?v=<?php echo time(); ?>">
+    <title>
+        <?php echo(get_window_title($title)); ?>
+    </title>
+    <style>
+        .list-item {
+            border: 2px dotted plum;
+        }
+        a {
+            color: purple;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <h2>Groovy Times Collective</h2>
+    <nav>
+        <?php 
+        // Kontrollera om användaren är inloggad och om roll är definierad
+        if (isset($_SESSION['auth']) && isset($_SESSION['auth']['role'])): 
+            $userRole = $_SESSION['auth']['role'];
+        var_dump($userRole);
+        ?>
+        
+            <?php if ($userRole == 'customer'): ?>
+                <a href="index.php">Home</a>
+                <a href="mySubscribers.php">My subscribers</a>
+                <a href="myNewsletter.php">My newsletters</a>
+                <a href="logout.php">Log out</a>
+            <?php elseif ($userRole == 'subscriber'): ?>
                 <a href="index.php">Home</a>
                 <a href="listNewsletters.php">All newsletters</a>
-                <a href="logIn.php">Log in</a>
-                <a href="createAccount.php">Create account</a>
+                <a href="mySubscriptions.php">My subscriptions</a>
+                <a href="logout.php">Log out</a>
             <?php endif; ?>
-        </nav>
-        </header>
+        <?php else: ?>
+            <a href="index.php">Home</a>
+            <a href="listNewsletters.php">All newsletters</a>
+            <a href="login.php">Log in</a>
+            <a href="createAccount.php">Create account</a>
+        <?php endif; ?>
+    </nav>
+</header>
